30fabf19decd447ab1bbdff77a477cbb2c58ffc0
Arnav <arnavdev404@gmail.com>
2020-03-14 19:41:02 -0700

    added rooms

--- index.js
+++ index.js
@@ -1,135 +1,93 @@
-var PORT = process.env.PORT || 8000
+var PORT = process.env.PORT || 5000
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var uuid = require('uuid');
 var https = require('https')
 
-var numberOfMembers = [4, 4, 4, 4];
+var numberOfMembers = [0, 0, 4, 3];
 var rooms = []
+var peopleLeft = {}
 
-server.listen(PORT);
+server.listen(5000);
 rooms[0] = uuid.v4()
 rooms[1] = uuid.v4()
 rooms[2] = uuid.v4()
 rooms[3] = uuid.v4()
 // WARNING: app.listen(80) will NOT work here!
 
-io.of('/26').on('connection', function (socket) {
-  socket.join(rooms[0])
-  numberOfMembers[0]++;
-  console.log("Number of People:" + numberOfMembers[0])
-  io.of('/26').emit("join", [numberOfMembers[0]])
-  if(numberOfMembers[0] === 5) {
-    https.get('https://opentdb.com/api.php?amount=5&category=26&type=multiple', (resp) => {
-      let data = '';
-
-      // A chunk of data has been recieved.
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-        console.log(rooms[0])
-        io.of('/26').emit('transition', JSON.parse(data).results)
-        numberOfMembers[0] = 4
-        rooms[0] = uuid.v4()
-      });
-
-    })
-  }
-  socket.on('disconnect', function(data) {
-    numberOfMembers[0]--;
-    io.of('/26').emit("join", [numberOfMembers[2]])
+io.on('connection', function (socket) {
+  console.log("CONNECTED")
+  var thing = 0
+  socket.on("winorloss", (change) => {
+    console.log(peopleLeft)
+    peopleLeft[change[1]]+=change[0]
+    console.log(peopleLeft[change[1]])
+    io.sockets.in(change[1]).emit("update", peopleLeft[change[1]])
   })
-});
 
-io.of('/23').on('connection', function (socket) {
-  socket.join(rooms[1])
-  numberOfMembers[1]++;
-  console.log("Number of People:" + numberOfMembers[1])
-  io.of('/23').emit("join", [numberOfMembers[1]])
-  if(numberOfMembers[1] === 5) {
-    https.get('https://opentdb.com/api.php?amount=5&category=23&type=multiple', (resp) => {
-      let data = '';
+  //When a client connects to lobby
+  socket.on('getCategory', (data123) => {
+    if(data123 == "26") {
+      thing = 0
+    }
+    if(data123 == "21") {
+      thing = 3
+    }
+    if(data123 == "17") {
+      thing = 2
+    }
+    if(data123 == "23") {
+      thing = 1
+    }
 
-      // A chunk of data has been recieved.
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
+    socket.join(rooms[thing])
+    numberOfMembers[thing]++;
+    io.to(rooms[thing]).emit("join"+data123, [numberOfMembers[thing]])
+    if(numberOfMembers[thing] === 5) {
+      https.get(`https://opentdb.com/api.php?amount=5&category=${data123}&type=multiple`, (resp) => {
+        let data = '';
+  
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+  
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          peopleLeft[rooms[thing]] = 5
+          io.emit('room'+data123, rooms[thing])
+          io.emit('transition'+data123, JSON.parse(data).results)
+          numberOfMembers[thing] = 0
+          rooms[thing] = uuid.v4()
+        });
+  
+      })
+    }
+  });
 
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-        console.log(rooms[1])
-        io.of('/23').emit('transition', JSON.parse(data).results)
-        numberOfMembers[1] = 4
-        rooms[1] = uuid.v4()
-      });
-
-    })
-  }
+  //DISCONNECT
   socket.on('disconnect', function(data) {
-    numberOfMembers[1]--;
-    io.of('/23').emit("join", [numberOfMembers[2]])
+    console.log("DISCONNECTED")
+    var data123 = 0
+    if(thing === 0) {
+      data123 = "26"
+    }
+    if(thing === 3) {
+      data123 = "21"
+    }
+    if(thing === 2) {
+      data123 = "17"
+    }
+    if(thing === 1) {
+      data123 = "23"
+    }
+    if(numberOfMembers[thing] > 0) {
+      socket.leave(rooms[thing])
+      numberOfMembers[thing]--;
+    }
+    io.to(rooms[thing]).emit("join"+data123, [numberOfMembers[thing]])
   })
-});
 
-io.of('/17').on('connection', function (socket) {
-  socket.join(rooms[2])
-  numberOfMembers[2]++;
-  console.log("Number of People:" + numberOfMembers[2])
-  io.of('/17').emit("join", [numberOfMembers[2]])
-  if(numberOfMembers[2] === 5) {
-    https.get('https://opentdb.com/api.php?amount=5&category=17&type=multiple', (resp) => {
-      let data = '';
 
-      // A chunk of data has been recieved.
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-        console.log(rooms[2])
-        io.of('/17').emit('transition', JSON.parse(data).results)
-        numberOfMembers[2] = 4
-        rooms[2] = uuid.v4()
-      });
-
-    })
-  }
-  socket.on('disconnect', function(data) {
-    numberOfMembers[2]--;
-    io.of('/17').emit("join", [numberOfMembers[2]])
-  })
-});
-
-io.of('/21').on('connection', function (socket) {
-  socket.join(rooms[3])
-  numberOfMembers[3]++;
-  io.of('/21').emit("join", [numberOfMembers[3]])
-  if(numberOfMembers[3] === 5) {
-    https.get('https://opentdb.com/api.php?amount=5&category=21&type=multiple', (resp) => {
-      let data = '';
-
-      // A chunk of data has been recieved.
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-        console.log(rooms[3])
-        io.of('/21').emit('transition', JSON.parse(data).results)
-        numberOfMembers[3] = 4
-        rooms[3] = uuid.v4()
-      });
-
-    })
-  }
-  socket.on('disconnect', function(data) {
-    numberOfMembers[3]--;
-    io.of('/21').emit("join", [numberOfMembers[2]])
-  })
 });
